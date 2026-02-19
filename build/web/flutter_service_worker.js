7fa56bec580387c9bb860f189d6299b16221fc25
mrjohn7751 <jahongirberdiyev7751@gmail.com>
2026-02-19 11:38:24 +0500

    done

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "67b7904a099c5046f0b0ffe5b3ab5439",
+const RESOURCES = {"flutter_bootstrap.js": "14f2508af403abad876530b0fd113180",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
