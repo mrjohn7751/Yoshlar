e5daf5d43e99a2d42bd7d110c6d3717306d8d732
mrjohn7751 <jahongirberdiyev7751@gmail.com>
2026-02-24 11:04:32 +0500

    done

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2646083220"
+    serviceWorkerVersion: "1661685801"
   }
 });
